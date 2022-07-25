02566f755a061f61839e4017cd31742f0b97314f
quiquelhappy <quiquelhappy@gmail.com>
2022-07-25 21:32:43 +0200

    landing

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 style="margin-bottom:5px">NOMINAL</h1>
+    <a href="mailto:hello@nominal.es">
+      <button>
+        <span>hiring</span>SEND CV
+      </button>
+    </a>
+    <a href="https://discord.gg/wUtrrHH6qx">
+      <button>
+        JOIN DISCORD
+      </button>
+    </a>
+  </div>
+</template>
