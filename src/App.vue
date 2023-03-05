d5485e90d2f9a34a15d46543955d840a544803e3
quiquelhappy <quiquelhappy@gmail.com>
2023-03-05 19:37:22 +0100

    make app bar width consistent accross products

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <v-app>
     <v-app-bar>
-      <v-row align="center" class="px-4">
-        <v-col cols="auto">
-          <v-app-bar-title style="font-weight: 900;">
-            <v-icon icon="mdi-atom" />nominal
-          </v-app-bar-title>
-        </v-col>
-        <v-col v-if="!$vuetify.display.mobile">
-          empowering the internet
-        </v-col>
-        <v-spacer v-else/>
-        <v-col cols="auto">
-          <v-btn v-if="$vuetify.display.mdAndUp" theme="dark" variant="tonal" color="primary"
-            prepend-icon="fa:fab fa-discord" to="/discord">
-            Discord
-          </v-btn>
-          <v-btn size="small" v-else theme="dark" variant="tonal" color="primary" icon="fa:fab fa-discord"
-            to="/discord" />
-        </v-col>
-      </v-row>
+      <v-container>
+        <v-row align="center" class="px-4">
+          <v-col cols="auto">
+            <v-app-bar-title style="font-weight: 900;">
+              <v-icon icon="mdi-atom" />nominal
+            </v-app-bar-title>
+          </v-col>
+          <v-col v-if="!$vuetify.display.mobile">
+            empowering the internet
+          </v-col>
+          <v-spacer v-else />
+          <v-col cols="auto">
+            <v-btn v-if="$vuetify.display.mdAndUp" theme="dark" variant="tonal" color="primary"
+              prepend-icon="fa:fab fa-discord" to="/discord">
+              Discord
+            </v-btn>
+            <v-btn size="small" v-else theme="dark" variant="tonal" color="primary" icon="fa:fab fa-discord"
+              to="/discord" />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-app-bar>
     <v-main>
       <router-view />
