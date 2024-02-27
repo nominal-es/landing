1613527b553dc2b32e38c904322d918e4fb510bd
quiquelhappy <quiquelhappy@gmail.com>
2024-02-27 19:28:28 +0100

    legal styling

--- src/views/Legal.vue
+++ src/views/Legal.vue
@@ -1,145 +1,177 @@
 <template>
-    <div>
-        <h1>Privacy Policy</h1>
-        <p>Effective Date: February 27th 2024</p>
-        <section>
-            <h2>1. Information We Collect</h2>
+    <div style="max-width: 1000px; width: 100%; margin-inline: auto">
+        <div class="legal">
+            <h1>Privacy Policy</h1>
+            <p>Effective Date: February 27th 2024</p>
+            <section>
+                <h2>1. Information We Collect</h2>
 
-            <h3>1.1 Personal Information</h3>
-            <ul>
-                <li>When you log in via TikTok, we collect information provided by TikTok for authentication purposes,
-                    including your TikTok username, profile picture, and email address associated with your TikTok account.
-                </li>
-                <li>We may collect additional personal information you provide voluntarily, such as your name, email
-                    address, and other profile information.</li>
-            </ul>
+                <h3>1.1 Personal Information</h3>
+                <ul>
+                    <li>When you log in via TikTok, we collect information provided by TikTok for authentication purposes,
+                        including your TikTok username, profile picture, and email address associated with your TikTok
+                        account.
+                    </li>
+                    <li>We may collect additional personal information you provide voluntarily, such as your name, email
+                        address, and other profile information.</li>
+                </ul>
 
-            <h3>1.2 Automatically Collected Information</h3>
-            <ul>
-                <li>We may automatically collect certain information about your device, browsing actions, and patterns,
-                    including but not limited to IP addresses, browser types, and operating systems.</li>
-            </ul>
-        </section>
+                <h3>1.2 Automatically Collected Information</h3>
+                <ul>
+                    <li>We may automatically collect certain information about your device, browsing actions, and patterns,
+                        including but not limited to IP addresses, browser types, and operating systems.</li>
+                </ul>
+            </section>
 
-        <section>
-            <h2>2. How We Use Your Information</h2>
-            <p>We use the information we collect, including TikTok login information, for the following purposes:</p>
-            <ul>
-                <li>To provide and personalize our services.</li>
-                <li>To improve our website and services based on your feedback.</li>
-                <li>To communicate with you, including responding to inquiries and sending important updates.</li>
-                <li>To ensure the security and integrity of our services.</li>
-            </ul>
-        </section>
+            <section>
+                <h2>2. How We Use Your Information</h2>
+                <p>We use the information we collect, including TikTok login information, for the following purposes:</p>
+                <ul>
+                    <li>To provide and personalize our services.</li>
+                    <li>To improve our website and services based on your feedback.</li>
+                    <li>To communicate with you, including responding to inquiries and sending important updates.</li>
+                    <li>To ensure the security and integrity of our services.</li>
+                </ul>
+            </section>
 
-        <section>
-            <h2>3. Sharing of Information</h2>
-            <p>We do not sell, trade, or otherwise transfer your personal information to third parties without your
-                consent, except as described in this Privacy Policy.</p>
-            <ul>
-                <li>We may share your information with third-party service providers who assist us in operating our website
-                    and services.</li>
-                <li>We may disclose your information in response to legal requests, enforce our policies, or protect our
-                    rights, privacy, safety, or property.</li>
-            </ul>
-        </section>
+            <section>
+                <h2>3. Sharing of Information</h2>
+                <p>We do not sell, trade, or otherwise transfer your personal information to third parties without your
+                    consent, except as described in this Privacy Policy.</p>
+                <ul>
+                    <li>We may share your information with third-party service providers who assist us in operating our
+                        website
+                        and services.</li>
+                    <li>We may disclose your information in response to legal requests, enforce our policies, or protect our
+                        rights, privacy, safety, or property.</li>
+                </ul>
+            </section>
 
-        <section>
-            <h2>4. Security</h2>
-            <p>We take reasonable measures to protect your personal information from unauthorized access, disclosure,
-                alteration, and destruction.</p>
-        </section>
+            <section>
+                <h2>4. Security</h2>
+                <p>We take reasonable measures to protect your personal information from unauthorized access, disclosure,
+                    alteration, and destruction.</p>
+            </section>
 
-        <section>
-            <h2>5. Your Choices</h2>
-            <p>You have the right to:</p>
-            <ul>
-                <li>Access and update your personal information.</li>
-                <li>Opt-out of receiving marketing communications.</li>
-                <li>Delete your account and personal information.</li>
-            </ul>
-        </section>
+            <section>
+                <h2>5. Your Choices</h2>
+                <p>You have the right to:</p>
+                <ul>
+                    <li>Access and update your personal information.</li>
+                    <li>Opt-out of receiving marketing communications.</li>
+                    <li>Delete your account and personal information.</li>
+                </ul>
+            </section>
 
-        <section>
-            <h2>6. Third-Party Links and Services</h2>
-            <p>Our website may contain links to third-party websites. Please be aware that we are not responsible for the
-                privacy practices of such third parties.</p>
-        </section>
+            <section>
+                <h2>6. Third-Party Links and Services</h2>
+                <p>Our website may contain links to third-party websites. Please be aware that we are not responsible for
+                    the
+                    privacy practices of such third parties.</p>
+            </section>
 
-        <section>
-            <h2>7. Changes to this Privacy Policy</h2>
-            <p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new
-                Privacy Policy on this page.</p>
-        </section>
+            <section>
+                <h2>7. Changes to this Privacy Policy</h2>
+                <p>We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new
+                    Privacy Policy on this page.</p>
+            </section>
 
-        <section>
-            <h2>8. Contact Us</h2>
-            <p>If you have any questions about this Privacy Policy, please contact us at <a
-                    href="mailto:contact@nominal.es">contact@nominal.es</a>.</p>
-        </section>
-    </div>
-    <div style="margin-top:90px">
-        <h1>Terms of service</h1>
-        <p>Effective Date: February 27th 2024</p>
-        <section>
-            <h2>1. Acceptance of Terms</h2>
-            <p>By accessing or using the services provided by Nominal.es, you agree to be bound by these Terms of Service.
-            </p>
-        </section>
+            <section>
+                <h2>8. Contact Us</h2>
+                <p>If you have any questions about this Privacy Policy, please contact us at <a
+                        href="mailto:contact@nominal.es">contact@nominal.es</a>.</p>
+            </section>
+        </div>
+        <div class="legal">
+            <h1>Terms of service</h1>
+            <p>Effective Date: February 27th 2024</p>
+            <section>
+                <h2>1. Acceptance of Terms</h2>
+                <p>By accessing or using the services provided by Nominal.es, you agree to be bound by these Terms of
+                    Service.
+                </p>
+            </section>
 
-        <section>
-            <h2>2. User Accounts and Registration</h2>
-            <p>To access certain features of Nominal.es, you may be required to create an account. You agree to provide
-                accurate and complete information during the registration process and to update such information to keep it
-                current.</p>
-        </section>
+            <section>
+                <h2>2. User Accounts and Registration</h2>
+                <p>To access certain features of Nominal.es, you may be required to create an account. You agree to provide
+                    accurate and complete information during the registration process and to update such information to keep
+                    it
+                    current.</p>
+            </section>
 
-        <section>
-            <h2>3. Use of Services</h2>
-            <p>You agree to use Nominal.es in compliance with all applicable laws and regulations. You are solely
-                responsible
-                for your conduct and any data, text, information, usernames, graphics, images, photos, profiles, audio and
-                video clips, links, and other content (collectively, "Content") that you submit, post, and display on
-                Nominal.es.</p>
-        </section>
+            <section>
+                <h2>3. Use of Services</h2>
+                <p>You agree to use Nominal.es in compliance with all applicable laws and regulations. You are solely
+                    responsible
+                    for your conduct and any data, text, information, usernames, graphics, images, photos, profiles, audio
+                    and
+                    video clips, links, and other content (collectively, "Content") that you submit, post, and display on
+                    Nominal.es.</p>
+            </section>
 
-        <section>
-            <h2>4. Intellectual Property</h2>
-            <p>The content on Nominal.es, including but not limited to text, graphics, logos, images, and software, is the
-                property of Nominal.es and is protected by copyright, trademark, and other intellectual property laws.</p>
-        </section>
+            <section>
+                <h2>4. Intellectual Property</h2>
+                <p>The content on Nominal.es, including but not limited to text, graphics, logos, images, and software, is
+                    the
+                    property of Nominal.es and is protected by copyright, trademark, and other intellectual property laws.
+                </p>
+            </section>
 
-        <section>
-            <h2>5. Termination of Services</h2>
-            <p>Nominal.es reserves the right to suspend or terminate your account and access to the services at its sole
-                discretion, with or without cause and without notice.</p>
-        </section>
+            <section>
+                <h2>5. Termination of Services</h2>
+                <p>Nominal.es reserves the right to suspend or terminate your account and access to the services at its sole
+                    discretion, with or without cause and without notice.</p>
+            </section>
 
-        <section>
-            <h2>6. Disclaimer of Warranties</h2>
-            <p>The services provided by Nominal.es are on an "as is" and "as available" basis. Nominal.es makes no
-                warranties, expressed or implied, regarding the accuracy, completeness, reliability, or suitability of the
-                services.</p>
-        </section>
+            <section>
+                <h2>6. Disclaimer of Warranties</h2>
+                <p>The services provided by Nominal.es are on an "as is" and "as available" basis. Nominal.es makes no
+                    warranties, expressed or implied, regarding the accuracy, completeness, reliability, or suitability of
+                    the
+                    services.</p>
+            </section>
 
-        <section>
-            <h2>7. Limitation of Liability</h2>
-            <p>Nominal.es and its affiliates shall not be liable for any indirect, incidental, special, consequential, or
-                punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss
-                of data, use, goodwill, or other intangible losses resulting from your access to or use of Nominal.es.</p>
-        </section>
+            <section>
+                <h2>7. Limitation of Liability</h2>
+                <p>Nominal.es and its affiliates shall not be liable for any indirect, incidental, special, consequential,
+                    or
+                    punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any
+                    loss
+                    of data, use, goodwill, or other intangible losses resulting from your access to or use of Nominal.es.
+                </p>
+            </section>
 
-        <section>
-            <h2>8. Governing Law</h2>
-            <p>These Terms of Service are governed by and construed in accordance with the laws of [Your Jurisdiction]. Any
-                legal suit, action, or proceeding arising out of or related to these Terms of Service shall be instituted
-                exclusively in the federal or state courts located in [Your Jurisdiction].</p>
-        </section>
+            <section>
+                <h2>8. Governing Law</h2>
+                <p>These Terms of Service are governed by and construed in accordance with the laws of [Your Jurisdiction].
+                    Any
+                    legal suit, action, or proceeding arising out of or related to these Terms of Service shall be
+                    instituted
+                    exclusively in the federal or state courts located in [Your Jurisdiction].</p>
+            </section>
 
-        <section>
-            <h2>9. Contact Us</h2>
-            <p>If you have any questions about these Terms of Service, please contact us at <a
-                    href="mailto:contact@nominal.es">contact@nominal.es</a>.</p>
-        </section>
+            <section>
+                <h2>9. Contact Us</h2>
+                <p>If you have any questions about these Terms of Service, please contact us at <a
+                        href="mailto:contact@nominal.es">contact@nominal.es</a>.</p>
+            </section>
+        </div>
     </div>
 </template>
+
+<style>
+.legal {
+    background-color: rgba(255, 255, 255, 0.05);
+    padding: 20px 40px;
+    margin-top: 50px;
+}
+
+h2 {
+    margin-top: 10px;
+}
+
+ul {
+    padding-left: 20px;
+}
+</style>
